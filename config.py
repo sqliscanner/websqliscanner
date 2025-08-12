e1842fca7e82c7cc331276237d16e8527be2e60a
Projectdatascience <projectdatascience864@example.com>
2025-08-13 01:26:40 +0700

    update

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'secret123')
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', '')
-    MYSQL_DB = os.getenv('MYSQL_DB', 'sqli_scanner')
+    MYSQL_HOST = os.getenv('MYSQLHOST', 'localhost')         
+    MYSQL_USER = os.getenv('MYSQLUSER', 'root')
+    MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD', '')
+    MYSQL_DB = os.getenv('MYSQLDATABASE', 'sqli_scanner')
